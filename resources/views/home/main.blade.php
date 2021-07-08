4000404ae19ef504694c5242303f303707da021f
Adam Chabrzyk <ch-adam@o2.pl>
2021-07-08 22:01:15 +0200

    Install laravel ui and bootstrap

--- resources/views/home/main.blade.php
+++ resources/views/home/main.blade.php
@@ -2,4 +2,11 @@
 
 @section('content')
     <h1>To jest strona startowa</h1>
+    <table class="table">
+        <tr>
+            <td>Lorem</td>
+            <td>ipsum</td>
+            <td>dolor</td>
+        </tr>
+    </table>
 @endsection
--- resources/views/layout/main.blade.php
+++ resources/views/layout/main.blade.php
@@ -7,8 +7,10 @@
     <meta name="description" content="Bezpłatna aplikacja do nauki słówek w twojej przeglądarce">
     <meta name="keywords" content="bezpłatna, aplikacja, przedkądarkowa, do, nauki, słówek, słownictwa">
     <meta name="author" content="Adam Chabrzyk">
-    <link rel="icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon"/>
     <title>@yield('title', 'Przeglądarkowa aplikacja do nauki słówek')</title>
+    <link rel="icon" href="{{ URL::asset('favicon.ico') }}" type="image/x-icon"/>
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <script src="{{ mix('js/app.js') }}"></script>
 </head>
 <body>
     <div class="container">
