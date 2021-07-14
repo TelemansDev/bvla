62346aa19566aff1a8b3b5420f5d42387b049a80
Adam Chabrzyk <ch-adam@o2.pl>
2021-07-14 18:25:00 +0200

    Create words table and seeder

---
+++ database/migrations/2021_07_14_122621_create_words_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWordsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('words', function (Blueprint $table) {
+            $table->id();
+            $table->string('name_en', 50);
+            $table->string('name_pl', 50);
+            $table->unsignedBigInteger('subcategory_id');
+
+            $table->foreign('subcategory_id')->references('id')->on('subcategories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('words');
+    }
+}
